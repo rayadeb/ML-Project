44c88edb735c1e2184b04e53bbee392e8da57510
rayadeb <rayadeb101@gmail.com>
2025-03-29 23:24:47 -0400

    fixed issues with headers in data, processed all data, and made modification to preprocessing program.

--- Preprocessing/preprocessing_ml.py
+++ Preprocessing/preprocessing_ml.py
@@ -2,11 +2,11 @@ import pandas as pd
 import os 
 
 # create new directory to store data
-output_dir = "Preprocessing/Preprocessed Data"
+output_dir = "Preprocessing\\Preprocessed Data"
 os.makedirs(output_dir, exist_ok=True)
 
 # loop through seasons from 2003-04 to 2023-24
-for year in range(2003, 2014): # modify this to change what files to filter
+for year in range(2003, 2024): # modify this to change what files to filter
     season = f"{year}-{str(year+1)[-2:]}"  # formats as "2003-04", "2004-05", etc.
     input_file = f"Preprocessing/Data/{season}.xlsx"  # construct file path
     output_file = f"{output_dir}/{season}_filtered.xlsx"  # output file with "_filtered" suffix
@@ -16,16 +16,23 @@ for year in range(2003, 2014): # modify this to change what files to filter
     # read both sheets
     regular_df = pd.read_excel(input_file, sheet_name="Regular")
     playoff_df = pd.read_excel(input_file, sheet_name="Playoff")
+    #drop league average rows
+    regular_df = regular_df.iloc[:-1]
+    playoff_df = playoff_df.iloc[:-1]
+
+
+    
 
     # filter regular season dataframe on playoff season dataframe to only keep players who played in the playoffs
     regular_df_filtered = regular_df[
         regular_df.set_index(['Player', 'Team']).index.isin(playoff_df.set_index(['Player', 'Team']).index)
     ]
-
-    # removing "awards" and "player additional" columns
-    regular_df_filtered = regular_df_filtered.iloc[:, :-2]
+    
+    # remove "awards" and "player additional" columns if they exist
+    regular_df_filtered = regular_df_filtered.drop(columns=['Awards', 'Player Additional'], errors='ignore')
 
     # checking to see if any entries in playoff_df are not present in filtered df
+
     missing_players = playoff_df[~playoff_df['Player'].isin(regular_df_filtered['Player'])]
     if not missing_players.empty:
         print(f"Missing players in {season}:")
